20e1ed25209c1ce13eb9e6c3018dc9246fc602ee
GAsNa <romane.leseur.pro@gmail.com>
2022-09-18 05:35:01 +0200

    supp ttt help

--- src/main/java/org/gasna/learningBDiscord/commands/CommandHelp.java
+++ src/main/java/org/gasna/learningBDiscord/commands/CommandHelp.java
@@ -13,7 +13,7 @@ public class CommandHelp implements CommandExecutor {
     public void run(MessageCreateEvent event, Command command, String[] args) {
         EmbedBuilder embed = get_embed_help("Help", "Discover the commands", "Enjoy!");
         embed.addInlineField("Ping the bot", "You can ping the bot with the command ``" + PREFIX + "ping`` or ``" + PREFIX +"p?``.")
-                .addInlineField("Tic-tac-toe", "You can play tic-tac-toe with: ``" + PREFIX + "3t``, ``" + PREFIX + "t3``, ``" + PREFIX + "ttt`` or ``" + PREFIX + "tic-tac-toe``. Kwon how to play with ``" + PREFIX + "t3 help`` or ``" + PREFIX + "t3 h``.")
+                //.addInlineField("Tic-tac-toe", "You can play tic-tac-toe with: ``" + PREFIX + "3t``, ``" + PREFIX + "t3``, ``" + PREFIX + "ttt`` or ``" + PREFIX + "tic-tac-toe``. Kwon how to play with ``" + PREFIX + "t3 help`` or ``" + PREFIX + "t3 h``.")
                 .addInlineField("Octogone sans regle!", "...");
         send_message(event, embed);
     }
